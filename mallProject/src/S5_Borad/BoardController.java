519ee3c07f02f4e7cbb73828fb7fdfab6d5e7dcd
whdidgma94 <whdidgma94@naver.com>
2023-01-27 11:20:50 +0900

    .
    .

--- mallProject/src/S5_Borad/BoardController.java
+++ mallProject/src/S5_Borad/BoardController.java
@@ -25,7 +25,37 @@ public class BoardController {
 		this.boardDAO = boardDAO;
 		mallController = MallController.getInstance();		
 	}
-
+	
+	void addBoard(String userName) {
+		System.out.print("제목 입력 : ");
+		String title = Util.scan.next();
+		System.out.print("내용 입력 : ");
+		String content = Util.scan.next();
+		Board board =new Board(title,content,userName);
+		boardDAO.addBoard(board);
+	}
+	
+	void removeBoard(String userName) {
+		for (int i = 0; i < boardDAO.getBoardList().size(); i++) {
+			if (userName.equals(boardDAO.getBoardList().get(i).userName)) {
+				System.out.printf("[%d] %s < 삭제 가능\n", i + 1, boardDAO.getBoardList().get(i).title);
+			} else {
+				System.out.printf("[%d] %s\n", i + 1, boardDAO.getBoardList().get(i).title);
+			}
+		}
+		System.out.print("삭제할 게시물 ");
+		int sel = Util.getInt()-1;
+		if(sel<0||sel>=boardDAO.getBoardList().size()) {
+			System.err.println("범위 오류");
+			return;
+		}
+		if(userName.equals(boardDAO.getBoardList().get(sel).userName)) {
+			boardDAO.removeBoard(sel);
+			return;
+		}
+		System.err.println("본인의 게시물이 아닙니다");
+	}
+	
 	public void menuBoard() {
 		while (true) {
 			System.out.println("[1]게시글 목록 [2]게시글 작성 [3]게시글 삭제 [0]뒤로 가기");
@@ -33,9 +63,9 @@ public class BoardController {
 			if (sel == 1) {
 				boardDAO.printBoard();
 			} else if (sel == 2) {
-				boardDAO.addBoard(mallController.getMemberLoginID());
+				addBoard(mallController.getMemberLoginID());
 			} else if (sel == 3) {
-				boardDAO.removeBoard(mallController.getMemberLoginID());
+				removeBoard(mallController.getMemberLoginID());
 			} else if (sel == 0) {
 				break;
 			} else {
--- mallProject/src/S5_Borad/BoardDAO.java
+++ mallProject/src/S5_Borad/BoardDAO.java
@@ -14,33 +14,17 @@ public class BoardDAO {
 		boardList = new ArrayList<Board>();
 	}
 
-	void addBoard(String userName) {
-		System.out.print("제목 입력 : ");
-		String title = Util.scan.next();
-		System.out.print("내용 입력 : ");
-		String content = Util.scan.next();
-		boardList.add(new Board(title,content,userName));
+	void addBoard(Board board) {		
+		boardList.add(board);
 	}
 
-	void removeBoard(String userName) {
-		for (int i = 0; i < boardList.size(); i++) {
-			if (userName.equals(boardList.get(i).userName)) {
-				System.out.printf("[%d] %s < 삭제 가능\n", i + 1, boardList.get(i).title);
-			} else {
-				System.out.printf("[%d] %s\n", i + 1, boardList.get(i).title);
-			}
-		}
-		System.out.print("삭제할 게시물 ");
-		int sel = Util.getInt()-1;
-		if(sel<0||sel>=boardList.size()) {
-			System.err.println("범위 오류");
-			return;
-		}
-		if(userName.equals(boardList.get(sel).userName)) {
-			boardList.remove(sel);
-			return;
-		}
-		System.err.println("본인의 게시물이 아닙니다");
+	public ArrayList<Board> getBoardList() {
+		return boardList;
+	}
+
+
+	void removeBoard(int sel) {
+		boardList.remove(sel);
 	}
 
 	void printBoard() {
